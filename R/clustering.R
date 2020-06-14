c2fac6166901a588a7ee78153e2a5e81c01270a4
rushil.chakra <rushil.chakra@gmail.com>
2020-06-13 22:14:14 -0700

    refactor(clustering): remove fviz_nbclust dependency

--- R/clustering.R
+++ R/clustering.R
@@ -4,7 +4,6 @@
 #'
 #' @aliases immunr_hclust immunr_kmeans immunr_dbscan
 #'
-#' @importFrom factoextra fviz_nbclust
 #' @importFrom stats kmeans as.dist cmdscale dist
 #'
 #' @description Cluster the data with one of the following methods:
@@ -159,7 +158,7 @@ dbscan <- function (data, eps, MinPts = 5, scale = FALSE, method = c("hybrid",
   out
 }
 
-# hclust and helper function .get_silinfo from factoextra
+# hclust and helper function .get_silinfo from factoextra package
 .get_silinfo <- function (cluster, diss) 
 {
   k <- length(unique(cluster))
@@ -213,6 +212,76 @@ hcut <- function (x, k = 2, isdiss = inherits(x, "dist"), hc_func = c("hclust",
   if (graph) 
     fviz_dend(hc)
   hc
+}
+
+# fviz_nbclust from factoextra package
+fviz_nbclust <- function (x, FUNcluster = NULL, method = c("silhouette", "wss", 
+                                                           "gap_stat"), diss = NULL, k.max = 10, nboot = 100, verbose = interactive(), 
+                          barfill = "steelblue", barcolor = "steelblue", linecolor = "steelblue", 
+                          print.summary = TRUE, ...) 
+{
+  set.seed(123)
+  if (k.max < 2) 
+    stop("k.max must bet > = 2")
+  method = match.arg(method)
+  if (!inherits(x, c("data.frame", "matrix")) & !("Best.nc" %in% 
+                                                  names(x))) 
+    stop("x should be an object of class matrix/data.frame or ", 
+         "an object created by the function NbClust() [NbClust package].")
+  if (inherits(x, "list") & "Best.nc" %in% names(x)) {
+    best_nc <- x$Best.nc
+    if (class(best_nc) == "numeric") 
+      print(best_nc)
+    else if (class(best_nc) == "matrix") 
+      .viz_NbClust(x, print.summary, barfill, barcolor)
+  }
+  else if (is.null(FUNcluster)) 
+    stop("The argument FUNcluster is required. ", "Possible values are kmeans, pam, hcut, clara, ...")
+  else if (!is.function(FUNcluster)) {
+    stop("The argument FUNcluster should be a function. ", 
+         "Check if you're not overriding the specified function name somewhere.")
+  }
+  else if (method %in% c("silhouette", "wss")) {
+    if (is.data.frame(x)) 
+      x <- as.matrix(x)
+    if (is.null(diss)) 
+      diss <- stats::dist(x)
+    v <- rep(0, k.max)
+    if (method == "silhouette") {
+      for (i in 2:k.max) {
+        clust <- FUNcluster(x, i, ...)
+        v[i] <- .get_ave_sil_width(diss, clust$cluster)
+      }
+    }
+    else if (method == "wss") {
+      for (i in 1:k.max) {
+        clust <- FUNcluster(x, i, ...)
+        v[i] <- .get_withinSS(diss, clust$cluster)
+      }
+    }
+    df <- data.frame(clusters = as.factor(1:k.max), y = v, 
+                     stringsAsFactors = TRUE)
+    ylab <- "Total Within Sum of Square"
+    if (method == "silhouette") 
+      ylab <- "Average silhouette width"
+    p <- ggpubr::ggline(df, x = "clusters", y = "y", group = 1, 
+                        color = linecolor, ylab = ylab, xlab = "Number of clusters k", 
+                        main = "Optimal number of clusters")
+    if (method == "silhouette") 
+      p <- p + geom_vline(xintercept = which.max(v), linetype = 2, 
+                          color = linecolor)
+    return(p)
+  }
+  else if (method == "gap_stat") {
+    extra_args <- list(...)
+    gap_stat <- cluster::clusGap(x, FUNcluster, K.max = k.max, 
+                                 B = nboot, verbose = verbose, ...)
+    if (!is.null(extra_args$maxSE)) 
+      maxSE <- extra_args$maxSE
+    else maxSE <- list(method = "firstSEmax", SE.factor = 1)
+    p <- fviz_gap_stat(gap_stat, linecolor = linecolor, maxSE = maxSE)
+    return(p)
+  }
 }
 
 immunr_hclust <- function(.data, .k = 2, .k.max = nrow(.data) - 1, .method = "complete", .dist = TRUE) {
